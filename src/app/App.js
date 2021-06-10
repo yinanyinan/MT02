ad812b70110ecbde0070775bb5aa439629458cdd
yinanyinan <yinan-huang@vorn.co.jp>
2021-06-10 15:36:12 +0900

    MT02-2

---
+++ src/app/App.js
@@ -0,0 +1,39 @@
+import React from 'angular';
+import Amplify from 'aws-amplify';
+import { AmplifyAuthenticator, AmplifySignUp, AmplifySignOut } from '@aws-amplify/ui-angular';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
+const App = () => {
+    const [authState, setAuthState] = React.useState();
+    const [user, setUser] = React.useState();
+
+    React.useEffect(() => {
+        return onAuthUIStateChange((nextAuthState, authData) => {
+            setAuthState(nextAuthState);
+            setUser(authData)
+        });
+    }, []);
+
+  return authState === AuthState.SignedIn && user ? (
+      <div className="App">
+          <div>Hello, {user.username}</div>
+          <AmplifySignOut />
+      </div>
+    ) : (
+      <AmplifyAuthenticator>
+        <AmplifySignUp
+          slot="sign-up"
+          formFields={[
+            { type: "username" },
+            { type: "password" },
+            { type: "email" }
+          ]}
+        />
+      </AmplifyAuthenticator>
+  );
+}
+
+export default App;
